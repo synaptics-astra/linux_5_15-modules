c6053d22e1e736b5d18771233b4c2460b0dd5b55
Jisheng Zhang <jszhang@kernel.org>
2024-05-08 14:49:07 +0800

    pinctrl-myna2: modularize
    Change-Id: Ibf2c187f802a734f6e573b45267ea4c0e53b4a46
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>
    Reviewed-on: https://sc-debu-git.synaptics.com/gerrit/224003
    Reviewed-by: Jisheng Zhang <Jisheng.Zhang@synaptics.com>

--- drivers/pinctrl/berlin/pinctrl-myna2/pinctrl-myna2.c
+++ drivers/pinctrl/berlin/pinctrl-myna2/pinctrl-myna2.c
@@ -8,10 +8,10 @@
  */
 
 #include <linux/init.h>
+#include <linux/module.h>
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
-#include <linux/module.h>
 
 #include "berlin.h"
 
@@ -536,6 +536,7 @@ static const struct of_device_id myna2_pinctrl_match[] = {
 	},
 	{}
 };
+MODULE_DEVICE_TABLE(of, myna2_pinctrl_match);
 
 static int myna2_pinctrl_probe(struct platform_device *pdev)
 {
@@ -586,11 +587,7 @@ static struct platform_driver myna2_pinctrl_driver = {
 		.of_match_table = myna2_pinctrl_match,
 	},
 };
-
-static int __init myna2_pinctrl_init(void)
-{
-	return platform_driver_register(&myna2_pinctrl_driver);
-}
-arch_initcall(myna2_pinctrl_init);
+module_platform_driver(myna2_pinctrl_driver);
 
 MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("pinctrl library for Synaptics myna2 SoC");
